6094ff1f315d706dbcb268495a867758ad7796fb
lucas <lucasgschons@gmail.com>
2021-12-28 16:49:41 -0300

    Mudanças gerais

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,29 +1,73 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
 
 import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
+
 interface Task {
-  id: number;
+  id: string;
   title: string;
   isComplete: boolean;
+  
 }
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  useEffect(() => {
+    console.log(tasks); 
+  }, [tasks])
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    if(newTaskTitle != ''){
+      
+      setNewTaskTitle(newTaskTitle)
+      
+      const { v4: uuidv4 } = require('uuid');
+      
+      setTasks((prevTasks) => [...prevTasks, 
+        {
+          id: uuidv4(),
+          title: newTaskTitle,
+          isComplete: false,
+      },]);
+      
+
+      
+    }else{
+      alert("Campo vazio não é permitido")
+    }
+
   }
 
-  function handleToggleTaskCompletion(id: number) {
+  function handleToggleTaskCompletion(id: string) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    tasks.map(task => {
+      if(task.id == id && task.isComplete == false){
+        const newList = tasks.filter((task) => task.isComplete = true);
+        setTasks(newList);
+
+      } else {
+        const newList = tasks.filter((task) => task.isComplete = false);
+        setTasks(newList);
+
+      }
+    })
+
   }
 
-  function handleRemoveTask(id: number) {
+  function handleRemoveTask(id: string) {
     // Remova uma task da listagem pelo ID
+    if(window.confirm('Tem certeza que você deseja excluir essa task?')){
+        
+      const newList = tasks.filter((task) => task.id !== id);
+      setTasks(newList);
+    
+      
+    }
   }
 
   return (
@@ -46,8 +90,9 @@ export function TaskList() {
 
       <main>
         <ul>
-          {tasks.map(task => (
-            <li key={task.id}>
+          {tasks.map(task => {
+            return (
+              <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
                   <input 
@@ -65,10 +110,14 @@ export function TaskList() {
                 <FiTrash size={16}/>
               </button>
             </li>
-          ))}
-          
+            );
+            })}        
         </ul>
       </main>
     </section>
   )
+}
+
+function uuidv4() {
+  throw new Error('Function not implemented.');
 }
